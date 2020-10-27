be39c0f5e9e11d1703f2996739b5f032331976db
JessMeLeblos <tensazangetsu456@gmail.com>
2020-10-27 15:32:45 -0600

    Fastter

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+    firebaseConfig : {
+    apiKey: "AIzaSyBRHH3aCVl7z3R3O9sMEevHgUHlGbSw7ck",
+    authDomain: "fastter-102ad.firebaseapp.com",
+    databaseURL: "https://fastter-102ad.firebaseio.com",
+    projectId: "fastter-102ad",
+    storageBucket: "fastter-102ad.appspot.com",
+    messagingSenderId: "13443228346",
+    appId: "1:13443228346:web:1d9158c1665347046a1883",
+    measurementId: "G-CT2C86X5YK"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
